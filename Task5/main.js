d65377bda25a67431eba61afaeb0c32cf2203db0
AhmadNajiKam <ahmadnajikam@gmail.com>
2024-10-15 23:32:37 +0300

    Add sum only for even numbers

--- Task5/main.js
+++ Task5/main.js
@@ -7,7 +7,7 @@ for(let i=num1;i<=num2;i++)
 function calculateArraySum(){
     fillNumbers(0,100);
     let sum=0;
-    for(let i=0;i<100;i++)sum+=nums[i];
+    for(let i=0;i<=100;i++)if(!(nums[i]%2))sum+=nums[i];
     return sum;
 } 
 function printArraySum(){
