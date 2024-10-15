e83f6ae71a1e78c40746c4d3895a8be1c6dadc67
AhmadNajiKam <ahmadnajikam@gmail.com>
2024-10-15 23:24:00 +0300

    Add a button that prints sum 0-100 when clicked

---
+++ Task4/main.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector("button");
+const nums =[];
+function fillNumbers(num1,num2){
+for(let i=num1;i<=num2;i++)
+    nums.push(i);
+}
+function calculateArraySum(){
+    fillNumbers(0,100);
+    let sum=0;
+    for(let i=0;i<100;i++)sum+=nums[i];
+    return sum;
+} 
+function printArraySum(){
+    console.log(calculateArraySum());
+}
+btn.addEventListener("click",printArraySum);
