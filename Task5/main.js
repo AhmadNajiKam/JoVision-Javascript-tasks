794704ce5217833447441cf75033fe7030840818
AhmadNajiKam <ahmadnajikam@gmail.com>
2024-10-15 23:37:21 +0300

    Fix array creation logic to meet the task criteria

--- Task5/main.js
+++ Task5/main.js
@@ -1,11 +1,13 @@
 const btn = document.querySelector("button");
-const nums =[];
+
 function fillNumbers(num1,num2){
+    const nums =[];
 for(let i=num1;i<=num2;i++)
     nums.push(i);
+return nums;
 }
 function calculateArraySum(){
-    fillNumbers(0,100);
+    const nums =  fillNumbers(0,100);
     let sum=0;
     for(let i=0;i<=100;i++)if(!(nums[i]%2))sum+=nums[i];
     return sum;
