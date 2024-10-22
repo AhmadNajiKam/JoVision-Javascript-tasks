b138b3415ae5ec37ae8e158976c70dbaf55c4d57
AhmadNajiKam <ahmadnajikam@gmail.com>
2024-10-22 14:12:30 +0300

    Solve Task12

---
+++ Task12/main.js
@@ -0,0 +1,15 @@
+"use strict"
+const btn = document.querySelector("button");
+const Age = document.querySelector("#Age");
+const Name = document.querySelector("#Name");
+function printData(){
+    const data ={};
+    data.name=Name.value;
+    data.age=Age.value;
+    Age.value='';
+    Name.value='';
+    data.time=Date();
+    alert(`Your name is: ${data.name}, Your age is: ${data.age},time is ${data.time}`);
+}
+
+btn.addEventListener("click",printData);
